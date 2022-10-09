c77e394868452c3b1c82a3ee47b56fc980e695b0
汤涛 <784933671@qq.com>
2022-10-09 11:34:25 +0800

    1

--- types/vite-env.d.ts
+++ types/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
-
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+/// <reference types="vue/macros-global" />
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
